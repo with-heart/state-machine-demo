24903c77f48bab999ff27e617b35ce9a9be01d2b
Mark Chandler <mark.allen.chandler@gmail.com>
2018-01-25 01:36:18 -0500

    Add App wrapper

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react'
+import Register from './components/Register'
 import styled from 'styled-components'
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${p => p.theme.colors.light};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: ${p => p.theme.border};
+  color: ${p => p.theme.colors.dark};
+`
 
-const Welcome = styled.h1``
+const Credits = styled.a`
+  font-size: 2rem;
+  font-weight: 900;
+  position: absolute;
+  bottom: 1rem;
+  right: 1rem;
+`
 
 class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Welcome>Welcome to Boilerplate or Whatever</Welcome>
+        <Register />
+        <Credits href="https://chanchan.io/">by ChanChan.</Credits>
       </Wrapper>
     )
   }

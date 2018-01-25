1b35eec8bd59fba194163143cb024e38073dcc54
Mark Chandler <mark.allen.chandler@gmail.com>
2018-01-24 23:07:30 -0500

    Create theme and inject global styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ThemeProvider, injectGlobal } from 'styled-components'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import theme from './theme'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+injectGlobal`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: system-ui;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root'),
+)
+registerServiceWorker()
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    dark: '#B177A3',
+    light: '#E3FEDD',
+  },
+  border: p => `5px solid ${p.theme.colors.dark}`,
+}
+
+export default theme
